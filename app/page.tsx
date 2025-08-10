159393d0d8c991b86e0eecee0ccc1ea951f0a04f
lhung <lhungdayy@gmail.com>
2025-08-10 17:46:13 +0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Lets build an API productivity APP</h1>
+
+      <Button>Click me</Button>
+    </div>
+  );
+}
