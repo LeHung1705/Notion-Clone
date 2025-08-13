b565bd6c46327aa80e0e4cb68253d4ea7d3ef22d
lhung <lhungdayy@gmail.com>
2025-08-13 08:49:28 +0700

    build header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 
 export const metadata: Metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <Header/>
         <body>
           {children}
         </body>
